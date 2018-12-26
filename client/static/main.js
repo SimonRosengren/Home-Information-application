8aac098a9ba1de7483ab9eac1bb1cab8eb40e0c6
simonrosengren <simonrosengren123@gmail.com>
2018-12-26 12:43:22 +0100

    print climate info

--- client/static/main.js
+++ client/static/main.js
@@ -1,4 +1,26 @@
+var tempText;
+var humidText;
 
-$.get('/temperature', (data, status) => {
-    console.log(data)
-})
+document.addEventListener("DOMContentLoaded", (event) => {
+    init();
+});
+
+function init() {
+    tempText = document.getElementById('temperature');
+    humidText = document.getElementById('humidity');
+
+    setInterval(printClimateData, 5000);
+}
+
+function printClimateData() {
+    fetch('/temperature')
+        .then(response => response.json())
+        .then(response => tempText.innerHTML = response);
+
+    fetch('/humidity')
+        .then(response => response.json())
+        .then(response => humidText.innerHTML = response);
+}
+
+
+
