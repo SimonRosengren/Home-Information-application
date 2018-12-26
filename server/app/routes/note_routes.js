87201d7e9b64598926bc40411352d70271c6f85a
simonrosengren <simonrosengren123@gmail.com>
2018-12-26 10:31:13 +0100

    init

---
+++ server/app/routes/note_routes.js
@@ -0,0 +1,46 @@
+
+var mqtt = require('mqtt');
+var temperature = "notset";
+var humidity = "notset";
+var options = {
+  port: 19116,
+  host: 'mqtt://m21.cloudmqtt.com',
+  clientId: 'mqttjs_' + Math.random().toString(16).substr(2, 8),
+  username: 'fdwpomyh',
+  password: 'a3LO4lnMiRAi',
+  keepalive: 60,
+  reconnectPeriod: 1000,
+  protocolId: 'MQIsdp',
+  protocolVersion: 3,
+  clean: true,
+  encoding: 'utf8'
+};
+
+
+function init() {
+  var client = mqtt.connect('mqtt://m21.cloudmqtt.com', options);
+  client.on('connect', function () { // When connected
+    console.log('connected');
+    client.subscribe('temperature');
+    client.subscribe('humidity');
+    client.on('message', function (topic, message, packet) {
+      if (topic == "temperature")
+        temperature = message;
+      if (topic == "humidity")
+        humidity = message;
+    });
+  });
+}
+
+init();
+
+module.exports = function (app, db) {
+  app.get('/temperature', (req, res) => {
+    res.send(temperature);
+  });
+  app.get('/humidity', (req, res) => {
+    res.send(humidity);
+  });
+};
+
+
